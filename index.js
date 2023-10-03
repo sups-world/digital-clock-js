c45d7a5e72bc67e85c4068ec45c76da6a1372f5c
Suprajna Sharma <suprasubedi11@gmail.com>
2023-10-03 15:07:59 +0545

    able to extract hours,minute and seconds

---
+++ index.js
@@ -0,0 +1,22 @@
+const hourElement = document.getElementById("hour");
+const minuteElement = document.getElementById("minutes");
+const secondElement = document.getElementById("seconds");
+const ampmElement = document.getElementById("ampm");
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
+
+  hourElement.innerText = h;
+  minuteElement.innerText = m;
+  secondElement.innerText = s;
+  ampmElement.innerText = ampm;
+}
+
+updateClock();
